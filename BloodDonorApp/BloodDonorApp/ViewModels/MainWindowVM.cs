291ef8dac64da088b550cc78bb63603bde2fdecc
alinblendea <alinblendea1708@gmail.com>
2021-11-11 16:36:06 +0200

    Helpers, Commands and Main Window VM
    Added a BaseVM as a helper, a RelayCommand file and a VM file for MainWindow; Also added buttons for each type of user on the main window and an exit button.

---
+++ BloodDonorApp/BloodDonorApp/ViewModels/MainWindowVM.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using BloodDonorApp.Commands;
+using BloodDonorApp.Views;
+
+namespace BloodDonorApp.ViewModels
+{
+    class MainWindowVM
+    {
+        private ICommand openWindowCommand;
+        public ICommand OpenWindowCommand
+        {
+            get
+            {
+                if (openWindowCommand == null)
+                {
+                    openWindowCommand = new RelayCommand(OpenWindow);
+                }
+                return openWindowCommand;
+            }
+        }
+
+        public void OpenWindow(object obj)
+        {
+            string nr = obj as string;
+            switch (nr)
+            {
+                case "1":
+                    MainWindow mainWindow = (Application.Current.MainWindow as MainWindow);
+                    Application.Current.MainWindow = new DonorWindow();
+                    Application.Current.MainWindow.Show();
+                    mainWindow.Close();
+                    break;
+
+                case "2":
+                    MainWindow mainWindow2 = (Application.Current.MainWindow as MainWindow);
+                    Application.Current.MainWindow = new MedicWindow();
+                    Application.Current.MainWindow.Show();
+                    mainWindow2.Close();
+                    break;
+
+                case "3":
+                    MainWindow mainWindow3 = (Application.Current.MainWindow as MainWindow);
+                    Application.Current.MainWindow = new StaffWindow();
+                    Application.Current.MainWindow.Show();
+                    mainWindow3.Close();
+                    break;
+
+                case "4":
+                    Process.GetCurrentProcess().Kill();
+                    break;
+            }
+        }
+    }
+}
