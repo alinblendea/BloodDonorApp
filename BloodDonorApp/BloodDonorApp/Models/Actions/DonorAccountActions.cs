d45db64a47ba5a86f692e6567ade0db164bba9f2
alinblendea <alinblendea1708@gmail.com>
2021-11-17 20:56:36 +0200

    Register functionality for donors
    Donors can now register an account

---
+++ BloodDonorApp/BloodDonorApp/Models/Actions/DonorAccountActions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using BloodDonorApp.Helpers;
+using BloodDonorApp.ViewModels;
+using BloodDonorApp.Views;
+
+namespace BloodDonorApp.Models.Actions
+{
+    class DonorAccountActions : BaseVM
+    {
+        BloodDonorEntities context = new BloodDonorEntities();
+
+        private DonorAccountVM donorAccountContext;
+        public DonorAccountActions(DonorAccountVM donorAccountContext)
+        {
+            this.donorAccountContext = donorAccountContext;
+        }
+
+        public void AddMethod(object obj)
+        {
+            DonorAccountVM donorAccountVM = obj as DonorAccountVM;
+            if (donorAccountVM != null)
+            {
+                if (String.IsNullOrEmpty(donorAccountVM.Email)
+                    || String.IsNullOrEmpty(donorAccountVM.Password)
+                    || String.IsNullOrEmpty(donorAccountVM.ConfirmPassword))
+                {
+                    donorAccountContext.Message = "Toate datele trebuie completate.";
+                    MessageBox.Show(donorAccountContext.Message);
+                }
+                else if(donorAccountVM.Password != donorAccountVM.ConfirmPassword)
+                {
+                    donorAccountContext.Message = "Parolele introduse nu coincid.";
+                    MessageBox.Show(donorAccountContext.Message);
+                }
+                else 
+                { 
+                    donorAccountVM.Type = "Donor";
+                    context.Conts.Add(new Cont() { id_cont = context.Conts.OrderByDescending(p => p.id_cont).FirstOrDefault().id_cont + 1, email = donorAccountVM.Email, parola = donorAccountVM.Password, type = "Donor"});
+                    context.SaveChanges();
+                    donorAccountContext.Message = "Cont creat cu succes!";
+                    MessageBox.Show(donorAccountContext.Message);
+                }
+            }
+        }
+
+        public void BackMethod(object obj)
+        {
+            DonorRegisterWindow mainWindow = (Application.Current.MainWindow as DonorRegisterWindow);
+            Application.Current.MainWindow = new DonorPreLoginWindow();
+            Application.Current.MainWindow.Show();
+            mainWindow.Close();
+        }
+    }
+}
---
+++ BloodDonorApp/BloodDonorApp/ViewModels/DonorAccountVM.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using BloodDonorApp.Commands;
+using BloodDonorApp.Helpers;
+using BloodDonorApp.Models.Actions;
+
+namespace BloodDonorApp.ViewModels
+{
+    class DonorAccountVM : BaseVM
+    {
+        DonorAccountActions pAct;
+
+        public DonorAccountVM()
+        {
+            pAct = new DonorAccountActions(this);
+        }
+
+        #region Data Members
+
+        private int id_cont;
+        private string email;
+        private string parola;
+        private string confirmedPassword;
+        private string type;
+        private string message;
+
+        public int AccId
+        {
+            get
+            {
+                return id_cont;
+            }
+            set
+            {
+                id_cont = value;
+                NotifyPropertyChanged("AccId");
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+                NotifyPropertyChanged("Email");
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return parola;
+            }
+            set
+            {
+                parola = value;
+                NotifyPropertyChanged("Password");
+            }
+        }
+
+        public string ConfirmPassword
+        {
+            get
+            {
+                return confirmedPassword;
+            }
+            set
+            {
+                confirmedPassword = value;
+                NotifyPropertyChanged("ConfirmPassword");
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+                NotifyPropertyChanged("Type");
+            }
+        }
+
+        public string Message
+        {
+            get
+            {
+                return message;
+            }
+            set
+            {
+                message = value;
+                NotifyPropertyChanged("Message");
+            }
+        }
+
+        #endregion
+
+        #region Command Members
+
+        private ICommand addCommand;
+        public ICommand AddCommand
+        {
+            get
+            {
+                if (addCommand == null)
+                {
+                    addCommand = new RelayCommand(pAct.AddMethod);
+                }
+                return addCommand;
+            }
+        }
+
+        private ICommand backCommand;
+        public ICommand BackCommand
+        {
+            get
+            {
+                if (backCommand == null)
+                {
+                    backCommand = new RelayCommand(pAct.BackMethod);
+                }
+                return backCommand;
+            }
+        }
+
+        #endregion
+    }
+}
