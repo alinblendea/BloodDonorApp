3f844ba553d69ec38b234d4852e3a6fd59bdbfdb
alinblendea <alinblendea1708@gmail.com>
2022-03-19 01:43:07 +0200

    Added benefit add
    Added the functionality to add more benefits, as a staff member.

---
+++ BloodDonorApp/BloodDonorApp/Models/Actions/BenefitsActions.cs
@@ -0,0 +1,64 @@
+﻿using BloodDonorApp.Helpers;
+using BloodDonorApp.ViewModels;
+using BloodDonorApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BloodDonorApp.Models.Actions
+{
+    class BenefitsActions : BaseVM
+    {
+        BloodDonorEntities context = new BloodDonorEntities();
+
+        private AddBenefitsVM benefitContext;
+        public BenefitsActions(AddBenefitsVM benefitContext)
+        {
+            this.benefitContext = benefitContext;
+        }
+
+        public void AddMethod(object obj)
+        {
+            BenefitAddWindow mainWindow = (Application.Current.MainWindow as BenefitAddWindow);
+            if(!String.IsNullOrEmpty(mainWindow.txtCompany.Text) &&
+               !String.IsNullOrEmpty(mainWindow.txtName.Text) &&
+               !String.IsNullOrEmpty(mainWindow.txtNr.Text) &&
+               !String.IsNullOrEmpty(mainWindow.txtCost.Text))
+            {
+                string company = mainWindow.txtCompany.Text;
+                string name = mainWindow.txtName.Text;
+                int nr = 0, cost = 0;
+
+                try
+                {
+                    nr = Int32.Parse(mainWindow.txtNr.Text);
+                    cost = Int32.Parse(mainWindow.txtCost.Text);
+                }
+                catch(Exception e)
+                {
+                    MessageBox.Show("Datele au fost introduse gresit!");
+                    return;
+                }
+
+                context.Benefits.Add(new Benefit() { id_beneficiu = context.Benefits.OrderByDescending(p => p.id_beneficiu).FirstOrDefault().id_beneficiu + 1, denumire = company + " " + name, nr_total = nr,  nr_ramase = nr, cost_per_buc = cost });
+                context.SaveChanges();
+                MessageBox.Show("Beneficiul a fost adaugat cu succes!");
+            }
+            else
+            {
+                MessageBox.Show("Toate datele trebuie completate!");
+            }
+        }
+
+        public void BackMethod(object obj)
+        {
+            BenefitAddWindow mainWindow = (Application.Current.MainWindow as BenefitAddWindow);
+            Application.Current.MainWindow = new BenefitsWindow();
+            Application.Current.MainWindow.Show();
+            mainWindow.Close();
+        }
+    }
+}
---
+++ BloodDonorApp/BloodDonorApp/ViewModels/AddBenefitsVM.cs
@@ -0,0 +1,128 @@
+﻿using BloodDonorApp.Commands;
+using BloodDonorApp.Helpers;
+using BloodDonorApp.Models.Actions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BloodDonorApp.ViewModels
+{
+    class AddBenefitsVM : BaseVM
+    {
+        BenefitsActions pAct;
+
+        public AddBenefitsVM()
+        {
+            pAct = new BenefitsActions(this);
+        }
+
+        #region Data Members
+
+        private int id_beneficiu;
+        private string denumire;
+        private int nr_total;
+        private int nr_ramase;
+        private int cost_per_buc;
+
+        public int BenefitId
+        {
+            get
+            {
+                return id_beneficiu;
+            }
+            set
+            {
+                id_beneficiu = value;
+                NotifyPropertyChanged("BenefitId");
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return denumire;
+            }
+            set
+            {
+                denumire = value;
+                NotifyPropertyChanged("Name");
+            }
+        }
+
+        public int TotalNo
+        {
+            get
+            {
+                return nr_total;
+            }
+            set
+            {
+                nr_total = value;
+                NotifyPropertyChanged("TotalNo");
+            }
+        }
+
+        public int RemainingNo
+        {
+            get
+            {
+                return nr_ramase;
+            }
+            set
+            {
+                nr_ramase = value;
+                NotifyPropertyChanged("RemainingNo");
+            }
+        }
+
+        public int Cost
+        {
+            get
+            {
+                return cost_per_buc;
+            }
+            set
+            {
+                cost_per_buc = value;
+                NotifyPropertyChanged("Cost");
+            }
+        }
+
+        #endregion
+
+        #region Command Members
+
+        private ICommand addCommand;
+        public ICommand AddCommand
+        {
+            get
+            {
+                if (addCommand == null)
+                {
+                    addCommand = new RelayCommand(pAct.AddMethod);
+                }
+                return addCommand;
+            }
+        }
+
+        private ICommand backCommand;
+        public ICommand BackCommand
+        {
+            get
+            {
+                if (backCommand == null)
+                {
+                    backCommand = new RelayCommand(pAct.BackMethod);
+                }
+                return backCommand;
+            }
+        }
+
+        #endregion
+    }
+}
