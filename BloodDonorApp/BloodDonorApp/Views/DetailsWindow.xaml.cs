d16cd32bde8ede46b62cc9b5d30734fd62fe987e
alinblendea <alinblendea1708@gmail.com>
2022-03-21 02:01:09 +0200

    Loading DetailsWindow info
    Loading the actual content of the DetailsWindow.

--- BloodDonorApp/BloodDonorApp/Views/DetailsWindow.xaml.cs
+++ BloodDonorApp/BloodDonorApp/Views/DetailsWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BloodDonorApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,11 +20,11 @@ namespace BloodDonorApp.Views
     /// </summary>
     public partial class DetailsWindow : Window
     {
-        public DetailsWindow(string mail, string cnp)
+        public DetailsWindow(string mail, string name, string cnp)
         {
             InitializeComponent(); 
             InitializeMail(mail);
-            InitializeInfo(cnp);
+            InitializeInfo(name, cnp);
         }
 
         public void InitializeMail(string mail)
@@ -31,9 +32,37 @@ namespace BloodDonorApp.Views
             txtmail.Text = mail;
         }
     
-        public void InitializeInfo(string cnp)
+        public void InitializeInfo(string name, string cnp)
         {
+            txtName.Text = name;
 
+            int donationsNumber = 0;
+            int contravaloare = 0;
+            List<int?> benefitIds = new List<int?>();
+
+            BloodDonorEntities context = new BloodDonorEntities();
+
+            List<Donare> donations = context.Donares.ToList();
+            foreach(Donare donation in donations)
+            {
+                if(donation.cnp_donator.Equals(cnp) && donation.isDone == true)
+                {
+                    donationsNumber++;
+                    benefitIds.Add(donation.id_beneficiu);
+                }
+            }
+
+            txtNo.Text = donationsNumber.ToString();
+            txtBenefits.Content = "-";
+
+            List<Benefit> benefits = context.Benefits.ToList();
+            foreach(int? id in benefitIds)
+            {
+                txtBenefits.Content = txtBenefits.Content + benefits[(int)id].denumire + "\n";
+                contravaloare += (int)benefits[(int)id].cost_per_buc;
+            }
+
+            txtPrice.Text = contravaloare.ToString();
         }
     }
 }
